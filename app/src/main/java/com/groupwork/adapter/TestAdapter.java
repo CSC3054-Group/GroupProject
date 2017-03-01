d5d47c405f7616b3f5c801a2b9aed7ff576e3427
40193632 <ltong02@qub.ac.uk>
2017-03-01 21:55:04 +0000

    Create java package

---
+++ app/src/main/java/com/groupwork/adapter/TestAdapter.java
@@ -0,0 +1,9 @@
+package com.groupwork.adapter;
+
+/**
+ * Created by admin on 2017/3/1.
+ */
+
+public class TestAdapter {
+
+}
---
+++ app/src/main/java/com/groupwork/customView/TestCustom.java
@@ -0,0 +1,9 @@
+package com.groupwork.customView;
+
+/**
+ * Created by admin on 2017/3/1.
+ */
+
+public class TestCustom {
+
+}
