7da4f8ad4d2357f64fc24520037536dae269fc6a
Gerard Cunningham <gcunningham12@qub.ac.uk>
2017-03-20 15:15:39 +0000

    Create a new review activity and ajoining xml file . renamed the xml file single_Point.xml to single_point.xml

---
+++ app/src/main/java/com/groupwork/activity/Reviewpage.java
@@ -0,0 +1,15 @@
+package com.groupwork.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.groupwork.R;
+
+public class Reviewpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviewpage);
+    }
+}
