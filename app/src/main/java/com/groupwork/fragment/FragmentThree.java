f8fc7be5e25f2b97053a4b3758aa07eef217a1c2
40192960 <jli31@qub.ac.uk>
2017-04-23 20:32:55 +0100

    add logout function for user profile page

--- app/src/main/java/com/groupwork/fragment/FragmentThree.java
+++ app/src/main/java/com/groupwork/fragment/FragmentThree.java
@@ -1,6 +1,8 @@
 package com.groupwork.fragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,13 +11,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.groupwork.R;
 import com.groupwork.activity.ForgotPassword;
+import com.groupwork.activity.HomeScreen;
 import com.groupwork.urlContrans.UrlConfig;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 /**
  * Created by admin on 2017/4/4.
@@ -28,6 +35,7 @@ public class FragmentThree extends Fragment {
     private TextView user_profile_password;
     private TextView user_profile_changepwd;
     private ImageView user_profile_img;
+    private Button user_profile_logout;
 
     @Nullable
     @Override
@@ -39,6 +47,7 @@ public class FragmentThree extends Fragment {
         user_profile_password = (TextView)view.findViewById(R.id.user_profile_password);
         user_profile_changepwd = (TextView)view.findViewById(R.id.user_profile_changepwd);
         user_profile_img = (ImageView)view.findViewById(R.id.user_profile_img);
+        user_profile_logout = (Button)view.findViewById(R.id.user_profile_logout);
 
         Log.d("forename", ""+UrlConfig.forename);
 
@@ -55,17 +64,62 @@ public class FragmentThree extends Fragment {
             }
         });
 
+        /*
         user_profile_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent("android.intent.action.PICK");
                 intent.setType("image/*");
+                startActivityForResult(intent,1);
+            }
+        });
+        */
+
+        user_profile_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HomeScreen.class);
                 startActivity(intent);
+                getActivity().finish();
             }
         });
 
         return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(data==null){
+            Toast.makeText(getActivity(),"empty",Toast.LENGTH_LONG).show();
+            return;//当data为空的时候，不做任何处理
+        }
+        Bitmap bitmap = null;
+        if(requestCode==1){
+            //获取从相册界面返回的缩略图
+            bitmap = data.getParcelableExtra("data");
+            Toast.makeText(getActivity(),"1",Toast.LENGTH_LONG).show();
+            if(bitmap==null){//如果返回的图片不够大，就不会执行缩略图的代码，因此需要判断是否为null,如果是小图，直接显示原图即可
+                /*
+                try {
+                    //通过URI得到输入流
+                    //InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                    //通过输入流得到bitmap对象
+                    //bitmap = BitmapFactory.decodeStream(inputStream);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                */
 
+            }
+        }else if(requestCode==2){
+            Toast.makeText(getActivity(),"2",Toast.LENGTH_LONG).show();
+            //bitmap = (Bitmap) data.getExtras().get("data");
+            //saveToSDCard(bitmap);
+        }
+        else{
+            Toast.makeText(getActivity(),"3",Toast.LENGTH_LONG).show();
+        }
+        //将选择的图片设置到控件上
+        user_profile_img.setImageBitmap(bitmap);
+    }
 }
