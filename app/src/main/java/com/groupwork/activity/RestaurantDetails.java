3ea269fa76c32aa284585cfe0f5f131ef652ed4a
Jamie <jmccully03@qub.ac.uk>
2017-04-25 15:45:28 +0100

    updated xml layout and added button handler

--- app/src/main/java/com/groupwork/activity/RestaurantDetails.java
+++ app/src/main/java/com/groupwork/activity/RestaurantDetails.java
@@ -38,7 +38,7 @@ import java.net.Socket;
 
 public class RestaurantDetails extends AppCompatActivity {
     TextView restname;
-    TextView restAddress;
+    //TextView restAddress;
     TextView restdesc;
     TextView opTimes;
     Button review;
@@ -61,7 +61,7 @@ public class RestaurantDetails extends AppCompatActivity {
 
                 UrlConfig.restaurantnumber = resphonenumber;
                 restname.setText(resname);
-                restAddress.setText(resaddress);
+                //restAddress.setText(resaddress);
                 opTimes.setText(resopeningtimes);
                 restdesc.setText(resdescription);
                 rating.setText(resreview);
@@ -73,7 +73,13 @@ public class RestaurantDetails extends AppCompatActivity {
             }
             else if (text.equals("ResNotSaved"))
             {   //not saved
-                saveres();
+                //saveres();
+            }
+            else if(text.equals("saved")){
+                Log.d("Test", "inserted successfully");
+                Toast.makeText(getApplication(), "Saved!", Toast.LENGTH_SHORT).show();
+
+
             }
         }
     };
@@ -85,28 +91,28 @@ public class RestaurantDetails extends AppCompatActivity {
         //UrlConfig.restid = "1";
 
         //Get id from previous activity
-        Intent intent  = getIntent();
-        int redId = intent.getIntExtra("resId",0);
+        Intent intent = getIntent();
+        int redId = intent.getIntExtra("resId", 0);
         UrlConfig.restid = String.valueOf(redId);
-        Log.d("ResID",String.valueOf(redId));
+        Log.d("ResID", String.valueOf(redId));
 
 
         setContentView(R.layout.activity_restaurant_details);
 
         //set variables so they can be assigned via query
         restname = (TextView) findViewById(R.id.txtRestaurantName);
-        restAddress = (TextView) findViewById(R.id.txtrestaurantaddress);
+        //restAddress = (TextView) findViewById(R.id.txtrestaurantaddress);
         //rOverall = (RatingBar) findViewById(R.id.ratingOverall);
         opTimes = (TextView) findViewById(R.id.txttimes);
         //number = (TextView) findViewById(R.id.textView16);
         rating = (TextView) findViewById(R.id.txtvrating);
         restdesc = (TextView) findViewById(R.id.txtDescription);
-        save = (Button)findViewById(R.id.btnsave);
-        call= (Button)findViewById(R.id.btncall);
-        review = (Button)findViewById(R.id.btnreview);
+        save = (Button) findViewById(R.id.btnsave);
+        call = (Button) findViewById(R.id.btncall);
+        review = (Button) findViewById(R.id.btnreview);
         call.setOnClickListener(myOnClick_call());
         review.setOnClickListener(myOnClick_review());
-        save.setOnClickListener(myOnClick_save());
+
 
         //use this id in query to select restaurant details from db and display them
         new Thread(new Runnable() {
@@ -126,7 +132,7 @@ public class RestaurantDetails extends AppCompatActivity {
                             "FROM tbl_restaurants " +
                             "INNER JOIN tbl_restdetails on tbl_restaurants.restId = tbl_restdetails.restId " +
                             "LEFT JOIN tbl_reviews on tbl_restaurants.restId = tbl_reviews.restId " +
-                            "WHERE tbl_restaurants.restId = '"+UrlConfig.restid+"'";
+                            "WHERE tbl_restaurants.restId = '" + UrlConfig.restid + "'";
                     pw.write(sqlString);
                     pw.flush();
                     socket.shutdownOutput();
@@ -183,146 +189,61 @@ public class RestaurantDetails extends AppCompatActivity {
             }
         }).start();
 
-    }
 
-    protected View.OnClickListener myOnClick_save() {
-        View.OnClickListener v = new View.OnClickListener() {
-
+        save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //save current restaurant as a saved place
-                //show toast to tell the user that the place has been saved
-                //run insert query
-                isSaved();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Socket socket = null;
 
-            }
-        };
-    return v;
-    }
-
-    public void isSaved(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Log.d("Test", "entered thread");
-                // open thread to process network access request and response
-                Socket socket = null;
-
-                try {
-                    socket = new Socket(UrlConfig.Socket_IP, UrlConfig.Socket_PORT);
-                    //Output data to Server
-                    OutputStream out = socket.getOutputStream();
-                    PrintWriter pw = new PrintWriter(out);
-                    //Input sql sentence which you want to execute
-                    String sqlString = "SELECT COUNT(restid) as cnt  FROM tbl_usersaved WHERE restid = '"+  UrlConfig.restid +"' " +
-                            "AND UserId= '"+ UrlConfig.userid +"')";
-                    pw.write(sqlString);
-                    pw.flush();
-                    socket.shutdownOutput();
-                    Log.d("Test", "transport successfully");
-
-
-                    //get response from server
-                    InputStream is = socket.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
-                    String info = "";
-                    String line = null;
-                    while ((line = br.readLine()) != null) {
-                        info = info + line;
-
-                    }
-
-                    //socket.shutdownInput();
-
-                    Log.d("Test", info);
-
-                    JSONArray array = new JSONArray(info);
-
-                    for (int i = 0; i < array.length(); i++) {
-                        JSONObject object = array.getJSONObject(i);
-
-                        int x = Integer.parseInt(object.getString("cnt"));
-
-                        if(x > 0){
-                            //already saved
+                        try {
+                            socket = new Socket(UrlConfig.Socket_IP, UrlConfig.Socket_PORT);
+                            //Output data to Server
+                            OutputStream out = socket.getOutputStream();
+                            PrintWriter pw = new PrintWriter(out);
+                            //Input sql sentence which you want to execute
+                            String sqlString = "INSERT INTO tbl_usersaved (restid, UserId) VALUES ('"+  UrlConfig.restid +"','"+ UrlConfig.userid +"')";
+                            pw.write(sqlString);
+                            pw.flush();
+                            socket.shutdownOutput();
 
                             Message message = Message.obtain();
-                            message.obj = "AlreadySaved";
-                            Log.d("AlreadySaved","AlreadySaved");
+                            message.obj = "saved";
+                            Log.d("resSaved","Saved");
                             message.what = 1;  // obj and what is similar as value-key
                             handler.sendMessage(message);// send message to handler
 
-                        }
-                        else{
 
-                            Message message = Message.obtain();
-                            message.obj = "ResNotSaved";
-                            Log.d("ResNotSaved","ResNotSaved");
-                            message.what = 1;  // obj and what is similar as value-key
-                            handler.sendMessage(message);// send message to handler
-                        }
+                        } catch (Exception e) {
 
-
-
-
-                        //Log.d("Test2",Email+);
-                    }
-                    Log.d("Test2", "successful");
-
-                } catch (Exception e) {
-
-                } finally {
-                    try {
-                        if (socket != null) {
-                            socket.close();
-                        }
+                        } finally {
+                            try {
+                                if(socket!=null){
+                                    socket.close();
+                                }
 //                                socket.shutdownInput();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
-    }
-
-    public void saveres(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Socket socket = null;
-
-                try {
-                    socket = new Socket(UrlConfig.Socket_IP, UrlConfig.Socket_PORT);
-                    //Output data to Server
-                    OutputStream out = socket.getOutputStream();
-                    PrintWriter pw = new PrintWriter(out);
-                    //Input sql sentence which you want to execute
-                    String sqlString = "INSERT INTO tbl_usersaved (restid, UserId) VALUES ('"+  UrlConfig.restid +"','"+ UrlConfig.userid +"')";
-                    pw.write(sqlString);
-                    pw.flush();
-                    socket.shutdownOutput();
-                    Log.d("Test", "inserted successfully");
-                    Toast.makeText(getApplication(), "Saved!", Toast.LENGTH_SHORT).show();
-
-
-                } catch (Exception e) {
-
-                } finally {
-                    try {
-                        if(socket!=null){
-                            socket.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
-//                                socket.shutdownInput();
-                    } catch (IOException e) {
-                        e.printStackTrace();
+
                     }
-                }
+                }).start();
+
 
             }
-        }).start();
+        });
+
 
     }
+
+
+
+
+
+
     protected View.OnClickListener myOnClick_review() {
         View.OnClickListener v = new View.OnClickListener() {
 
