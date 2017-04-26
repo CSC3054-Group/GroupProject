3c4b47f4c1b329a3c85573fdf8724d4d6b2d032f
Harry Wilson <hwilson20@qub.ac.uk>
2017-04-26 12:44:54 +0100

    more prgress on review page

--- app/src/main/java/com/groupwork/activity/Reviewpage.java
+++ app/src/main/java/com/groupwork/activity/Reviewpage.java
@@ -2,14 +2,52 @@ package com.groupwork.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.groupwork.R;
+import com.groupwork.urlContrans.UrlConfig;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Reviewpage extends AppCompatActivity {
+    RatingBar ratebar;
+    EditText review;
+    Button submitreview;
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        review = (EditText) findViewById(R.id.txtaddreview);
+        ratebar = (RatingBar) findViewById(R.id.ratingPrice);
+        submitreview = (Button) findViewById(R.id.buttonsub);
+        submitreview.setOnClickListener(myOnClick_submitreview());
         setContentView(R.layout.activity_reviewpage);
+    }
+
+    protected View.OnClickListener myOnClick_submitreview() {
+        View.OnClickListener v = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //save review typed into textbox
+                //shave review given on ratingbar
+                //run insert query
+            }
+        }
     }
 }
